4f2567ced8a95aee48319e1670100c505fa65fe9
aderama2711 <ade.ramadha@gmail.com>
2023-05-11 12:00:33 +0700

    add wget for go

--- Install/install-go.sh
+++ Install/install-go.sh
@@ -1,2 +1,3 @@
+wget https://go.dev/dl/go1.20.4.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.20.4.linux-amd64.tar.gz
 sudo echo "export PATH=\$PATH:/usr/local/go/bin" >> /etc/profile
